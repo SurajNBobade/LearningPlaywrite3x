d3895041a3a440ceeab799c71b8f1c5fce78c35f
SurajNBobade <surajbobade60@gmail.com>
2026-07-18 23:02:43 +0530

    Restructure chapters and add identifier/JavaScript content
    - Remove old flat chapter files (chapter_01_Basics)
    
    - Add organized chapter directories: 01_Chapter_Javascript, 02_Chapter_Javascript, 03_Chapter_Identifier
    
    - Add IQ notes for identifier concepts
    
    Co-authored-by: CommandCodeBot <noreply@commandcode.ai>

---
+++ 03_Chapter_Identifier/05_Comments.js
@@ -0,0 +1,16 @@
+// This is sinle comment this will be ignore 
+// this line will be not executed
+
+/*
+ *  This is multi line
+ *  Author : Suraj Bobade
+ *  Date : 18-July-2026
+ */
+
+/**
+ *  This is multi line
+ *  Author : Suraj Bobade
+ *  Date : 18-July-2026
+ **/
+
+var g = 10; // cmd + /, ctr + /
